48e36f3a7a85bfca33f14f0729efbeea8d5e4ee4
CHANMOON PARK <thechanmoon@gmail.com>
2020-02-14 03:45:59 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -43,12 +43,12 @@ def naming_system
   # HoH all the way to Species!
   return
   {
-      :Kingdom  => PORTION_1,
-      :Phylum   => {},
-      :Class    => {},
-      :Order    => PORTION_2,
-      :Family   => PORTION_3,
-      :Genus    => {},
-      :Species  => PORTION_4,
+    label: "Kingdom",
+    sub_category: {
+      label: "Phylum",
+      sub_category: {
+        label: "Class"
+      }
+    }
   }
 end
