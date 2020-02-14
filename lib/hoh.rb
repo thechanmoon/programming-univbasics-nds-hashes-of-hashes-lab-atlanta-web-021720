d141fe29205b96556844e5b1a6b3d604c9c4a5f8
CHANMOON PARK <thechanmoon@gmail.com>
2020-02-14 03:48:22 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -47,7 +47,10 @@ def naming_system
     sub_category: {
       label: "Phylum",
       sub_category: {
-        label: "Class"
+        label: "Order"
+        sub_category: {
+          label: "Class"
+          }
       }
     }
   }
