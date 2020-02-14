784ed2cc5868d3fe1fed86e54fee29551b89f113
CHANMOON PARK <thechanmoon@gmail.com>
2020-02-14 04:00:08 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -41,7 +41,7 @@ def naming_system
   #  Species
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
-  {
+  retVal = {
     label: "Kingdom",
     sub_category: {
       label: "Phylum",
@@ -63,4 +63,5 @@ def naming_system
       } 
     }
   }
+  return retVal
 end
